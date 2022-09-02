dad9cd398f91eb6f1eca5a56e7999d89c4278dfb
Simranjeet7860 <54342505+Simranjeet7860@users.noreply.github.com>
2022-09-02 11:52:48 +0530

    Add files via upload

---
+++ library_management/library_management/library_management/doctype/library_settings/test_library_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Simranjeet Singh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLibrarySettings(FrappeTestCase):
+	pass
